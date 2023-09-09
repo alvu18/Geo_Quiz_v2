98ab08898056f1835bb59f489a240ec97648a3c5
alvu18 <118918898+alvu18@users.noreply.github.com>
2023-09-09 10:54:00 +0300

    Создал проект и выполнил часть шагов

---
+++ app/src/main/java/com/example/geo_quiz_v2/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.geo_quiz_v2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+    private var currentIndex = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener { view: View ->
+            Toast.makeText(
+                this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        falseButton.setOnClickListener { view: View ->
+            Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/geo_quiz_v2/Question.kt
@@ -0,0 +1,4 @@
+package com.example.geo_quiz_v2
+
+import androidx.annotation.StringRes
+data class Question(@StringRes val textResId: Int, val answer: Boolean)
